cd02e891eb7139f7beb470cc6a9bc957e1332dbe
hejun <hejun@bilibili.com>
2016-04-01 22:47:19 +0800

    init project

---
+++ command-line/lottery.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'integers', metavar='int', nargs='+', type=int,
+        help='an integer to be summed')
+
+if __name__ == '__main__':
+    print 1
